075ac0270b7a6c9400f015aaf31d5be1bdfd415a
yukyk <yusufkhambaty1@gmail.com>
2026-04-14 23:12:01 +0530

    Used Variables for util.js

--- Utils/util.js
+++ Utils/util.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME || 'expense_tracker', process.env.DB_USER || 'root', process.env.DB_PASS || 'YusufgitSharp@25321453', {
-  host: process.env.DB_HOST || '127.0.0.1',
-  dialect: process.env.DB_DIALECT || 'mysql',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER , process.env.DB_PASS , {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
   port: parseInt(process.env.DB_PORT) || 3306,
   pool: {
     max: 20,
